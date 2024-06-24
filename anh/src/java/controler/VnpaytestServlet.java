99d8f9ce45a498f08932b84af3f4b2656cc12b25
Anhmn03 <anhmnhe176487@fpt.edu.vn>
2024-06-24 23:22:36 +0700

    cái này sai nha

---
+++ anh/src/java/controler/VnpaytestServlet.java
@@ -0,0 +1,231 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controler;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dal.DiscountDAO;
+import dal.EventDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+import model.Event;
+
+/**
+ *
+ * @author hoangduc
+ */
+public class VnpaytestServlet extends HttpServlet {
+
+    EventDAO evd = new EventDAO();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet VnpaytestServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet VnpaytestServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String event_id = request.getParameter("event_id");
+        String amount_raw = request.getParameter("amount");
+        String amount = amount_raw.split("\\.")[0];
+        String quantity = request.getParameter("quantity");
+        String status = request.getParameter("status");
+        try {
+//            event_id= Integer.parseInt(event_id_raw);
+//            amount=Integer.parseInt(integerPart);
+//            quantity= Integer.parseInt(quantity_raw);
+//            DAO_event paynemt = new DAO_event();
+            Event event = evd.getEventById(event_id);
+            request.setAttribute("event", event);
+            request.setAttribute("amount", amount);
+            request.setAttribute("quantity", quantity);
+            request.setAttribute("status", status);
+            request.getRequestDispatcher("payment_ticket.jsp").forward(request, response);
+        } catch (Exception e) {
+        }
+        String discountCode = request.getParameter("discountCodeInput");
+        
+        
+        if (discountCode != null && discountCode.trim() != "") {
+            String err = "";
+            int discountAmount = Integer.parseInt(amount);
+            try {
+                DiscountDAO d = new DiscountDAO();
+                if (d.checkDiscountByCode(discountCode)) {
+                    discountAmount = (int) (discountAmount * 0.9);  
+                    err="Bạn đã áp mã giảm giá thành công";
+                } else {
+                    err = "Mã giảm giá không hợp lệ ";
+                    
+                }
+            } catch (NumberFormatException e) {
+                err = "Lỗi chuyển đổi giá trị giảm giá";
+                e.printStackTrace();
+            }            
+            request.setAttribute("err", err);
+            request.setAttribute("discountAmount", discountAmount);
+        }
+        request.getRequestDispatcher("payment_ticket.jsp").forward(request, response);
+        return;
+        
+
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String user_name = request.getParameter("username");
+        String status = request.getParameter("status");
+//         if(user_name==null){
+//          req.getRequestDispatcher("sign_in.jsp").forward(req, resp);
+//         }
+        String vnp_Version = "2.1.0";
+        String vnp_Command = "pay";
+        String orderType = "other";
+        long amount = Integer.parseInt(request.getParameter("amount")) * 100;
+        String bankCode = request.getParameter("bankCode");
+
+        String vnp_TxnRef = Config.getRandomNumber(8);//mã đơn hàng // có thể thay bằng id vé
+        String vnp_IpAddr = Config.getIpAddress(request);
+
+        String vnp_TmnCode = Config.vnp_TmnCode;
+
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", vnp_Version);
+        vnp_Params.put("vnp_Command", vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", "VND");
+
+        if (bankCode != null && !bankCode.isEmpty()) {
+            vnp_Params.put("vnp_BankCode", bankCode);
+        }
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:");
+        vnp_Params.put("vnp_OrderType", orderType);
+
+        String locate = request.getParameter("language");
+        if (locate != null && !locate.isEmpty()) {
+            vnp_Params.put("vnp_Locale", locate);
+        } else {
+            vnp_Params.put("vnp_Locale", "vn");
+        }
+        String event_id = request.getParameter("event_id");
+
+        vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl + "?" + "event_id=" + event_id + "&" + "user_name=" + user_name + "&status=" + status);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+
+        String queryUrl = query.toString();
+        String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+        com.google.gson.JsonObject job = new JsonObject();
+        job.addProperty("code", "00");
+        job.addProperty("message", "success");
+        job.addProperty("data", paymentUrl);
+        Gson gson = new Gson();
+        response.getWriter().write(gson.toJson(job));
+        response.sendRedirect(paymentUrl);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ anh/src/java/dal/DiscountDAO.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Discount;
+
+/**
+ *
+ * @author Admin
+ */
+public class DiscountDAO extends DBContext {
+
+    public boolean addDiscount(String code, String quantity) {
+        String sql = "INSERT INTO [dbo].[Promotion]\n"
+                + "           ([code]\n"
+                + "           ,[quantity])\n"
+                + "     VALUES\n"
+                + "           (?,?)";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, code);
+            st.setString(2, quantity);
+            st.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("err" + e.getMessage());
+        }
+        return false;
+    }
+
+    public List<Discount> getALLDiscount() {
+        List<Discount> listD = new ArrayList<>();
+        String sql = "SELECT [id]\n"
+                + "      ,[code]\n"
+                + "      ,[quantity]\n"
+                + "  FROM [dbo].[Promotion]";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Discount dis;
+                String id, code, quantity;
+                id = String.valueOf(rs.getInt("id"));
+                code = rs.getString("code");
+                quantity = String.valueOf(rs.getInt("quantity"));
+                dis = new Discount(id, code, quantity);
+                listD.add(dis);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return listD;
+    }
+
+    public boolean  checkDiscountByCode(String code) {
+        String sql = "SELECT [id]\n"
+                + "      ,[code]\n"
+                + "      ,[quantity]\n"
+                + "  FROM [dbo].[Promotion] "
+                + "WHERE [code] = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+             st.setString(1, code);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                return true;
+            }
+        }catch (SQLException e) {
+            System.out.println(e);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        List<Discount> listD = new ArrayList<>();
+        DiscountDAO d = new DiscountDAO();
+        
+        if(d.checkDiscountByCode("ABC123")){
+            System.out.println("123");
+        }
+//        listD = d.getALLDiscount();
+//        String testcode = "Happy";
+//        String testquantity = "20";
+//        boolean isAdd = d.addDiscount(testcode, testquantity);
+//
+//        if (isAdd) {
+//            System.out.println("success");
+//
+//        } else {
+//            System.out.println("failed");
+//        }
+//
+//        listD = d.getALLDiscount();
+//        for (Discount discount : listD) {
+//            System.out.println(discount);
+//        }
+
+    }
+
+}
